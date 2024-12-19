a885f61596eee4f605abd84b33d2f12835e1a921
Bharat-Mishra <86016295+Bharatm231@users.noreply.github.com>
2024-12-19 16:00:06 +0530

    first approach
    In this landing page it is compatible with every screen size device and the best part is each device is divided in two parts of the screen screen size.

---
+++ script.js
@@ -0,0 +1,53 @@
+// // const months=['january', 'februrey', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'noveber', 'december'];
+// // const fileteredMonths=months.filter((month,index,arr)=>{
+// //     // console.log(index+':'+month);
+// //     return !month.includes('m');
+// // });
+// // console.log(fileteredMonths);
+
+// const Students=[
+//     {
+//         name:'bharat',
+//         age:22
+//     },
+//     {
+//         name:'ajay',
+//         age:20
+//     },
+//     {
+//         name:'arak',
+//         age:17
+//     },
+//     {
+//         name:'yash',
+//         age:11
+//     }
+// ]
+// const filteredStudents=Students.filter((stud,index)=>{
+//     console.log(stud.age);
+//     return stud.age>18;
+// })
+// console.log(filteredStudents);
+// function add(){
+//     let sum=[];
+//     for(let i=0;i<arguments.length;i++){
+//         Object.assign(sum,arguments);
+//     }
+//     return sum;
+// }
+// // console.log(sum);
+
+// const nums=[1,2,3,4,5,6,7,8,9];
+// const sum=(...nums)=>{
+//     return nums.reduce((accumulator,current)=>{
+//         return accumulator+current;
+//     },0);
+     
+// };
+// function average(){
+//     let sum=0;
+//     for(let i=0;i<arguments.length;i++){
+//         sum+=arguments[i];
+//     }
+//     return sum;
+// }
